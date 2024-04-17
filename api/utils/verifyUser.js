52afe143f1a26eef1d51119d364e62cace1f68f3
Vishnu-dholu <dholuvishnu10@gmail.com>
2024-04-17 10:31:44 +0530

    complete update user functionality

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,14 +1,19 @@
 import jwt from 'jsonwebtoken';
 import { errorHandler } from './error.js';
 
+//  Middleware function to verify JWT token
 export const verifyToken = (req, res, next) => {
+    //  Extracting JWT token from request cookies
     const token = req.cookies.access_token;
-
+    //  If token is not present, return unauthorized error 
     if (!token) return next(errorHandler(401, 'Unauthorized'));
 
+    //  Verify the JWT token
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        //  If verification fails, return forbidden error
         if (err) return next(errorHandler(403, 'Forbidden'));
 
+        //  If verification succeeds, attach user information to request object
         req.user = user;
         next();
     })
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,33 +2,38 @@ import { useEffect, useRef, useState } from 'react';
 import {useSelector} from 'react-redux';
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage';
 import { app } from '../firebase';
+import { updateUserStart, updateUserSuccess, updateUserFailure } from '../redux/user/userSlice';
+import { useDispatch } from 'react-redux';
 
 export default function Profile() {
-  const fileRef = useRef(null)
-  const {currentUser} = useSelector((state) => state.user)
-  const [file, setFile] = useState();
-  const [filePerc, setFilePerc] = useState(0);
-  const [fileUploadError, setFileUploadError] = useState(false);
-  const [formData, setFormData] = useState({});
-  console.log(formData);
-  console.log(filePerc);
-  console.log(fileUploadError);
+  const fileRef = useRef(null)  //  Reference to file input element
+  const {currentUser, error} = useSelector((state) => state.user)  //  Current user data from Redux store
+  const [file, setFile] = useState(); //  State for selected file
+  const [filePerc, setFilePerc] = useState(0);  //  State for file upload progress
+  const [fileUploadError, setFileUploadError] = useState(false);  //   State for file upload error
+  const [formData, setFormData] = useState({});   //  State for form data
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const dispatch = useDispatch(); //  Dispatch function for Redux actions
 
+  //  Effect to handle file upload when 'file' state changes
   useEffect(() => {
     if(file) {
       handleFileUpload();
     }
   }, [file]);
 
+  //  Function to handle file upload to Firebase Storage
   const handleFileUpload = () => {
     if (file) {
-        const storage = getStorage(app);
-        const fileName = new Date().getTime() + file.name;
-        const storageRef = ref(storage, fileName);
-        const uploadTask = uploadBytesResumable(storageRef, file);
+        const storage = getStorage(app);  //  Firebase Storage instance
+        const fileName = new Date().getTime() + file.name;  //  Generate unique file name
+        const storageRef = ref(storage, fileName);  //  Reference to the file in Firebase Storage
+        const uploadTask = uploadBytesResumable(storageRef, file);  //  Upload task for the file 
 
+        //  Event listeners for upload task
         uploadTask.on('state_changed',
             (snapshot) => {
+              //  Update file upload progress
                 const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
                 setFilePerc(Math.round(progress));
             },
@@ -36,8 +41,10 @@ export default function Profile() {
                 setFileUploadError(true);
             },
             () => {
+              //  On upload completion, get the download URL of the uploaded file
               getDownloadURL(uploadTask.snapshot.ref).then
               ((downloadURL) => {
+                //  Update form data with the download URL of the uploaded file
                 setFormData({ ...formData, avatar: downloadURL });
               })
             }
@@ -45,12 +52,43 @@ export default function Profile() {
     }
 };
 
+  //  Function to handle form input change
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id] : e.target.value });
+  }
+  //  Function to handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());  //  Dispatch action to indicate user update start
+      const res = await fetch(`/api/user/update/${currentUser._id}`,{
+        //  Send POST request to update user data
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData), //  Send form data in JSON format
+      });
+      const data = await res.json();  //  Parse response data
+      if (data.success === false) {
+        //  If update fails, dispatch failue action
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+
+      //  If update successful, dispatch success action
+      dispatch(updateUserSuccess(data));
+    } catch (error) {
+      //  If an error occurs, dispatch failure action
+      dispatch(updateUserFailure(error.message));
+    }
+  }
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='my-7 text-3xl font-semibold text-center'>Profile</h1>
 
-      <form className='flex flex-col gap-4'>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input onChange={(e) => setFile(e.target.files[0])}
         type='file' ref={fileRef} hidden accept='image/*' />
 
@@ -75,9 +113,11 @@ export default function Profile() {
           }
         </p>
 
-        <input type='text' placeholder='username' 
+        <input type='text' placeholder='username' defaultValue={currentUser.username}
+        onChange={handleChange}
          id='username' className='p-3 border rounded-lg'/>
-        <input type='email' placeholder='email' 
+        <input type='email' placeholder='email' defaultValue={currentUser.email}
+        onChange={handleChange}
          id='email' className='p-3 border rounded-lg'/>
         <input type='password' placeholder='password' 
          id='password' className='p-3 border rounded-lg'/>
@@ -89,6 +129,11 @@ export default function Profile() {
         <span className='text-red-700 cursor-pointer'>Delete account</span>
         <span className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
+      
+      <p className='text-red-700 mt-5'>{error ? error : ''}</p>
+      <p className='text-green-700 mt-5'>
+        {updateSuccess ? 'User is updated successfully!' : ''}
+      </p>
     </div>
   )
 }
