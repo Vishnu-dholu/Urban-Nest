9a1f624c7c69bb6c37ee1098846ae04115a2bab6
Vishnu-dholu <dholuvishnu10@gmail.com>
2024-04-19 15:10:42 +0530

    complete listing page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -268,7 +268,9 @@ export default function CreateListing() {
               />
               <div className="flex flex-col items-center">
                 <p>Regular price</p>
-                <span className="text-xs">(₹ / month)</span>
+                {formData.type === 'rent' && (
+                  <span className="text-xs">(₹ / month)</span>
+                )}
               </div>
             </div>
             <div className="flex items-center gap-2">
@@ -284,7 +286,9 @@ export default function CreateListing() {
               />
               <div className="flex flex-col items-center">
                 <p>Discounted price</p>
-                <span className="text-xs">(₹ / month)</span>
+                {formData.type === 'rent' && (
+                  <span className="text-xs">(₹ / month)</span>
+                )}
               </div>
             </div>
           </div>
--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -284,7 +284,9 @@ export default function CreateListing() {
               />
               <div className="flex flex-col items-center">
                 <p>Regular price</p>
-                <span className="text-xs">(₹ / month)</span>
+                {formData.type === 'rent' && (
+                  <span className="text-xs">(₹ / month)</span>
+                )}
               </div>
             </div>
             <div className="flex items-center gap-2">
@@ -300,7 +302,9 @@ export default function CreateListing() {
               />
               <div className="flex flex-col items-center">
                 <p>Discounted price</p>
-                <span className="text-xs">(₹ / month)</span>
+                {formData.type === 'rent' && (
+                  <span className="text-xs">(₹ / month)</span>
+                )}
               </div>
             </div>
           </div>
