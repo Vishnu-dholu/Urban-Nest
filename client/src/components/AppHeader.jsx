9cf1a95a406f7fab906047c26447ccaf03b13a7b
Vishnu-dholu <dholuvishnu10@gmail.com>
2024-04-21 21:23:08 +0530

    complete header search form functionality

--- client/src/components/AppHeader.jsx
+++ client/src/components/AppHeader.jsx
@@ -1,9 +1,28 @@
 import {useSelector} from 'react-redux';
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import {FaSearch} from 'react-icons/fa'
+import { useEffect, useState } from 'react';
 
 export default function AppHeader() {
     const {currentUser} = useSelector(state => state.user)
+    const [searchTerm, setSearchTerm] = useState('');
+    const navigate = useNavigate();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const urlParams = new URLSearchParams(window.location.search);
+        urlParams.set('searchTerm', searchTerm);
+        const searchQuery = urlParams.toString();
+        navigate(`/search?${searchQuery}`);
+    };
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFromUrl = urlParams.get('searchTerm');
+        if (searchTermFromUrl) {
+            setSearchTerm(searchTermFromUrl);
+        }
+    }, [location.search]);
+    
   return (
     <header className='bg-slate-200 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -14,11 +33,13 @@ export default function AppHeader() {
                 </h1>
             </Link>
 
-            <form className='p-3 bg-slate-100 rounded-lg flex items-center'>
+            <form onSubmit={handleSubmit} className='p-3 bg-slate-100 rounded-lg flex items-center'>
                 <input 
                     type='text'
                     placeholder='Search...'
                     className='bg-transparent focus:outline-none w-24 sm:w-64'
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
                 />
                 <button>
                     <FaSearch className='text-slate-600' />
