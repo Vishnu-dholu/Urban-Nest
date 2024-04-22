933d00c0f7e84210076b335e1f0b2eed4e2d30aa
Vishnu-dholu <dholuvishnu10@gmail.com>
2024-04-22 17:53:22 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 // Loading environment variables from .env file
 dotenv.config();
@@ -17,6 +18,8 @@ mongoose.connect(process.env.MONGODB_URI)
     .catch((err) => {
         console.error('Error connecting to MongoDB:', err);
     });
+
+    const __dirname = path.resolve();
 
 // Creating an Express application
 const app = express();
@@ -34,6 +37,12 @@ app.use('/api/user', userRouter);
 // Mounting authRouter at '/api/auth' endpoint
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', index.html));
+})
 
 // Error handling middleware to handle errors passed to next() function
 app.use((err, req, res, next) => {
